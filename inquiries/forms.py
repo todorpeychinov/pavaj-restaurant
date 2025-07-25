9aeaabda1a970dc4812de0f9affb608009766ced
Todor Peychinov <161106514+todorpeychinov@users.noreply.github.com>
2025-07-25 12:10:24 +0300

    Added custom error messages for privacy policy.

--- inquiries/forms.py
+++ inquiries/forms.py
@@ -12,7 +12,10 @@ class InquiryBaseForm(forms.ModelForm):
 class InquiryCreateForm(InquiryBaseForm):
     confirm_policy = forms.BooleanField(
         required=True,
-        label="I confirm that I have read and agree to the Privacy Policy and Data Protection Terms."
+        label="I confirm that I have read and agree to the Privacy Policy and Data Protection Terms.",
+        error_messages={
+            'required': 'You must agree to the Privacy Policy and Data Protection Terms in order to proceed.'
+        },
     )
 
 
